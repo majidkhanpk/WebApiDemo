a9b427347e6b0ca08ad67cf16ff5c627ec262270
Majid Khan <majidkhanqau@gmail.com>
2026-05-02 04:23:05 +0500

    Initial setup of controller based api

---
+++ WebApiDemo/Controllers/ShirtsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShirtsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetShirts()
+        {
+            return "Get all Shirts";
+        }
+
+        [HttpGet("{id}")]
+        public string GetShirtById(int id)
+        {
+            return $"Reading Shirt {id}";
+        }
+
+        [HttpPost]
+        public string CreateShirt()
+        {
+            return "Create Shirt";
+        }
+
+        [HttpPut("{id}")]
+        public string UpdateShirt(int id)
+        {
+            return $"Update Shirt {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteShirt(int id)
+        {
+            return $"Delete Shirt {id}";
+        }
+    }
+}
